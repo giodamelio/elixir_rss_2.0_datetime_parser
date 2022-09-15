b239a92168bdedeb17df0b5308ca8ea1a30221d1
Gio d'Amelio <giodamelio@gmail.com>
2022-09-15 20:52:50 +0000

    mix new

---
+++ lib/rss20_datetime_parser.ex
@@ -0,0 +1,18 @@
+defmodule Rss20DatetimeParser do
+  @moduledoc """
+  Documentation for `Rss20DatetimeParser`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Rss20DatetimeParser.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/rss20_datetime_parser_test.exs
@@ -0,0 +1,8 @@
+defmodule Rss20DatetimeParserTest do
+  use ExUnit.Case
+  doctest Rss20DatetimeParser
+
+  test "greets the world" do
+    assert Rss20DatetimeParser.hello() == :world
+  end
+end
